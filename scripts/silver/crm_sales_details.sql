af63329b24cc4874cb737e91e42d35c60099bd7c
Eduardo <eduardortelles@outlook.com>
2026-03-06 16:05:46 -0300

    Update crm_sales_details.sql

--- scripts/silver/crm_sales_details.sql
+++ scripts/silver/crm_sales_details.sql
@@ -8,21 +8,7 @@ Processamento: Limpeza e Transformação (CRM Sales Details)
 ---------------------------------------------------------------------------------
 */
 
-IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL 
-    DROP TABLE silver.crm_sales_details;
-
-CREATE TABLE silver.crm_sales_details (
-    sls_order_num   NVARCHAR (50),
-    sls_prd_key     NVARCHAR (50),
-    sls_cust_id     INT,
-    sls_order_dt    DATE,
-    sls_ship_dt     DATE,
-    sls_due_dt      DATE,
-    sls_sales       INT,
-    sls_quantity    INT,
-    sls_price       INT,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
-);
+TRUNCATE TABLE silver.crm_sales_details;
 
 INSERT INTO silver.crm_sales_details (
     sls_order_num,
